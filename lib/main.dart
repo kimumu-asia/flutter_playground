5395706b8a777a1137660816eb6ae88b482f5d29
Seolah Kim <>
2024-09-20 13:16:17 +0900

    GridView에 이미지 적용해보기

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,118 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Hello flutter!~~2~!', style: TextStyle(fontSize: 40)),
-            centerTitle: true,
-          ),
-          body: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(36),
-                    child: Image.network(
-                        "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
-                        width: 81),
-                  ),
-                  TextField(decoration: InputDecoration(labelText: '이메일')),
-                  TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(labelText: '비밀번호')),
-                  Container(
-                      width: double.infinity,
-                      margin: EdgeInsets.only(top: 16),
-                      child:
-                          ElevatedButton(onPressed: () {}, child: Text('로그인')))
-                ],
+      home: MyFoodReceipe(),
+    );
+  }
+}
+
+class FoodItem {
+  String title;
+  String imageUrl;
+
+  FoodItem({required this.title, required this.imageUrl});
+}
+
+class MyFoodReceipe extends StatelessWidget {
+  List<FoodItem> items = [
+    FoodItem(
+      title: '수제버거',
+      imageUrl: 'assets/images/burger.png',
+    ),
+    FoodItem(
+      title: '건강식',
+      imageUrl: 'assets/images/health.jpg',
+    ),
+    FoodItem(
+      title: '햄버거',
+      imageUrl: 'assets/images/originalBurger.jpg',
+    ),
+    FoodItem(
+      title: '한식',
+      imageUrl: 'assets/images/koreanFood.jpg',
+    ),
+    FoodItem(
+      title: '디저트',
+      imageUrl: 'assets/images/dessert.jpg',
+    ),
+    FoodItem(
+      title: '피자',
+      imageUrl: 'assets/images/pizza.jpg',
+    ),
+  ];
+
+  String query = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Food Recipe'),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                // 눌렀을때 핸들러
+              })
+        ],
+      ),
+      body: Column(
+        children: [
+          // * 검색창
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                  autocorrect: true,
+                  decoration: InputDecoration(
+                      labelText: 'Search',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.search)))),
+          // * 그리드뷰
+          Expanded(
+            child: GridView.builder(
+              padding: EdgeInsets.all(7.0),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // 그리드의 열 개수
+                crossAxisSpacing: 2.0, // 열 간격
+                mainAxisSpacing: 2.0, // 행 간격
               ),
+              itemCount:
+                  items.where((item) => item.title.contains(query)).length,
+              itemBuilder: (context, index) {
+                var filteredItems =
+                    items.where((item) => item.title.contains(query)).toList();
+                return Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(2.0), // 둥근 모서리 반경 설정
+                  ),
+                  elevation: 2,
+                  child: Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image:
+                            AssetImage(filteredItems[index].imageUrl), // 이미지 경로
+                        fit: BoxFit.cover, // background-size: cover처럼 적용
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        filteredItems[index].title,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-          )),
+          ),
+        ],
+      ),
     );
   }
 }

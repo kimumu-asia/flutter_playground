8fb066c340d485231ad6dd1b213c0a31417b7a88
Seolah Kim <>
2025-01-15 17:14:41 +0900

    컬러 팔레트 관리 파일 추가
      * https://www.figma.com/design/kEg4NzgwqPBEFKyIfalwty/%EC%82%AC%EC%9A%A9%EC%9E%90%EC%95%B1-UI-Design

---
+++ lib/theme/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  // private constructor를 선언함으로서 개발자가 실수로 인스턴스화하는 코드를 작성하는 실수 방지
+  AppColors._();
+
+  static const transparent = Color.fromRGBO(0, 0, 0, 0);
+
+  static const primaryGreen = Color.fromRGBO(28, 174, 129, 1.0);
+
+  static const neutralBlack = Color.fromRGBO(28, 174, 129, 1.0);
+  static const neutralGray = Color.fromRGBO(28, 174, 129, 1.0);
+  static const neutralBackgroundBlue = Color.fromRGBO(251, 253, 255, 1.0);
+
+  static const accentPrimaryMainYellow = Color.fromRGBO(255, 188, 0, 1.0);
+  static const accentPrimaryMainRed = Color.fromRGBO(239, 48, 36, 1.0);
+  static const accentPrimaryMainOrange = Color.fromRGBO(255, 162, 107, 1.0);
+  static const accentPrimaryMainBlue = Color.fromRGBO(0, 136, 255, 1.0);
+}
+
+// NOTE: 팔레트가 많아지는 경우 테마별로 컬러 클래스를 나누는 것을 고려중
+
+// abstract class PrimaryColors {
+// }
+
+// abstract class NeutralColors {
+// }
